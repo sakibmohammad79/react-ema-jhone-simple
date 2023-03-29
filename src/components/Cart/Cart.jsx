84e38951644869461fb936ea47f5e0eb0e43a58a
Mohammad Sakib <mohammadsakib7679@gmail.com>
2023-03-30 01:39:53 +0600

    display data to loclaStorage in cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,19 +4,24 @@ import './Cart.css';
 const Cart = ({cart}) => {
     
     //console.log(cart);
-
+    
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        totalPrice = totalPrice + product.price;
+        // if(product.quantity == 0){
+        //     product.quantity = 1;
+        //  }
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = (totalPrice*7)/100;
     const grandTotal = totalPrice + tax + totalShipping;
     return (
         <div className='cart'>
             <h2>Order Summary</h2>
-            <p>Selected items: {cart.length}</p>
+            <p>Selected items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping: ${totalShipping}</p>
             <p>Tax: ${tax}</p>
